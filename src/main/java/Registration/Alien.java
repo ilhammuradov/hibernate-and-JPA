c753accb8ee84f4f0f64dfa091ad70f9a9e22357
ilhammuradov <muradovilham039@gmail.com>
2024-06-24 14:26:35 +0400

    Registration

---
+++ src/main/java/Registration/Alien.java
@@ -0,0 +1,45 @@
+package Registration;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity()
+@Table(name = "Aliens")
+public class Alien {
+    @Id
+    private int id;
+    private AlienName name;
+    @Column(name = "alien_colour")
+    private String colour;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public AlienName getName() {
+        return name;
+    }
+
+    public void setName(AlienName name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Registration.Alien{" + "id=" + id + ", name='" + name + '\'' + ", colour='" + colour + '\'' + '}';
+    }
+}
