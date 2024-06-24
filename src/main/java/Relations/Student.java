d84751449f132d9fc5b571f3f07f208078f13e82
ilhammuradov <muradovilham039@gmail.com>
2024-06-24 14:28:42 +0400

    Relations

---
+++ src/main/java/Relations/Student.java
@@ -0,0 +1,55 @@
+package Relations;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int marks;
+    @ManyToMany(mappedBy = "studentList")
+    private List<Laptop> laptop = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public List<Laptop> getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(List<Laptop> laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name='" + name + '\'' + ", marks=" + marks + ", laptop=" + laptop + '}';
+    }
+}
